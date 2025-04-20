18a0d590dae4e649f39b0eda41555674e6b207c6
ayyzenn <saad.ahmad1024@gmail.com>
2025-04-20 21:31:02 +0500

    updating the volume in i3block

--- config/i3blocks/volume.sh
+++ config/i3blocks/volume.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
-# Get volume info
+# Handle clicks
+case "$BLOCK_BUTTON" in
+    1)  # Left click – Toggle mute
+        amixer set Master toggle > /dev/null
+        ;;
+    4)  # Scroll up – Volume up
+        amixer set Master 5%+ > /dev/null
+        ;;
+    5)  # Scroll down – Volume down
+        amixer set Master 5%- > /dev/null
+        ;;
+esac
+
+# Get mute status and volume
 amixer get Master | grep -q '\[off\]'
 IS_MUTED=$?
 
 VOLUME=$(amixer get Master | awk -F'[][]' '/%/ { print $2; exit }' | tr -d '%')
 
+# Choose icon
 if [ $IS_MUTED -eq 0 ]; then
-    ICON="🔇"  # muted
+    ICON="🔇"
 elif [ "$VOLUME" -eq 0 ]; then
-    ICON="🔈"  # very low
+    ICON="🔈"
 elif [ "$VOLUME" -le 50 ]; then
-    ICON="🔉"  # medium
+    ICON="🔉"
 else
-    ICON="🔊"  # high
+    ICON="🔊"
 fi
 
 echo "$ICON $VOLUME%"
+
