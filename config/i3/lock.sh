cf3c8fe9b4b4ce7463f0f5a0ca3ebec9cfbd6a88
ayyzenn <saad.ahmad1024@gmail.com>
2025-05-01 00:10:22 +0500

    adding the lock.sh file and updating the i3 config file for that

---
+++ config/i3/lock.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Get battery percentage
+battery=$(cat /sys/class/power_supply/BAT*/capacity)
+
+# greeter_text="Endless Night'\n' ${battery}%"
+greeter_text="Endless Night | ${battery}%"
+
+# Launch i3lock
+i3lock \
+  --blur 7 \
+  --clock \
+  --indicator \
+  --radius 100 \
+  --ring-width 6 \
+  --inside-color=00000088 \
+  --ring-color=ffffffaa \
+  --line-color=00000000 \
+  --keyhl-color=ffcc00ff \
+  --bshl-color=ff0000ff \
+  --separator-color=00000000 \
+  --time-color=ffffffff \
+  --date-color=aaaaaaff \
+  --layout-color=ffffffff \
+  --verif-color=89b4faFF \
+  --wrong-color=ff0000ff \
+  --time-font="Iosevka Nerd Font" \
+  --date-font="JetBrains Mono" \
+  --time-str="%I:%M:%S %p" \
+  --date-str="%a, %b %d" \
+  --greeter-text="$greeter_text" \
+  --greeter-color=ffffffff \
+  --greeter-font="Iosevka Nerd Font" \
+  --greeter-pos="ix:iy+150"
+
