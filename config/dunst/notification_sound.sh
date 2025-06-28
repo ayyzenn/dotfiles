78bf735a4695580f081f0ce5c942604ae747fb8e
ayyzenn <saad.ahmad1024@gmail.com>
2025-06-28 13:39:36 +0500

    upading the sound notification

--- config/dunst/notification_sound.sh
+++ config/dunst/notification_sound.sh
@@ -9,6 +9,14 @@ BODY="$3"
 ICON="$4"
 URGENCY="$5"
 
+# Skip sound for Slack (it has its own notification sounds)
+# Check for various Slack app name formats
+case "$APPNAME" in
+    "slack"|"Slack"|"Slack Desktop"|"com.slack.Slack"|"org.gtk.Slack"|".slack-wrapped")
+        exit 0
+        ;;
+esac
+
 # Define sound files based on urgency
 case "$URGENCY" in
     "CRITICAL")
